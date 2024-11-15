29d998ed5b42b989256071054ee3a397ebd8239d
e-Manny <espinozamanny95@gmail.com>
2024-11-14 19:02:17 -0800

    adding simple, hard-coded pet post functionality

--- frontend/src/pages/ShelterManagement.jsx
+++ frontend/src/pages/ShelterManagement.jsx
@@ -1,11 +1,15 @@
 // Profile management screen for shelters
 import React from "react";
 import Layout from "../components/Layout.jsx";
+import PetForm from "../components/PetForm.jsx";
 
 const ShelterManagement = () => {
-  return <Layout footerType="default">
-    {/* Page content here */}
-  </Layout>
+  return (
+    <Layout footerType="default">
+      {/* adding PetForm here just for tesitng post route */}
+      <PetForm />
+    </Layout>
+  );
 };
 
 export default ShelterManagement;
